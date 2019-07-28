462804b96b0d029fd9b9c502d39bfeb1afd28f36
David Munro <david.munro@nokia.com>
2019-07-28 07:56:11 -0400

    Updates

---
+++ precache-manifest.37c96b677b41e4e39a1dcf56dc44a09b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4e4f82de214b1fac6c9ad962b30de4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bff6bc56e6692be9346",
+    "url": "/static/css/main.5f55626a.chunk.css"
+  },
+  {
+    "revision": "14e88f7c99d21213ffda",
+    "url": "/static/js/2.2d6538c5.chunk.js"
+  },
+  {
+    "revision": "7bff6bc56e6692be9346",
+    "url": "/static/js/main.4cc156af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
